c1b2ec4d40ee8725bd697c6c9e696d5abd9ac285
laiszerbetto-source <laiszerbetto@gmail.com>
2026-05-08 14:20:16 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React, { useState, useRef, useEffect } from 'react';
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInAnonymously, onAuthStateChanged } from 'firebase/auth';
